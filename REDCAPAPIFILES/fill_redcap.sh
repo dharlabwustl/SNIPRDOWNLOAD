788265f302375b2e3422bd49c28b40fdc3b53a84
dharlabwustl <atulkumar@wustl.edu>
2024-01-11 23:46:17 +0800

    Commit 1

--- REDCAPAPIFILES/fill_redcap.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-redcap_token=${REDCAP_API_TOKEN}
-redcap_url='https://redcap.wustl.edu/redcap/api/'
-python3 redcapapi.py ${redcap_token} ${redcap_url}
--- download_ct_scans.py
+++ download_ct_scans.py
@@ -1,3 +1,3 @@
 import os,glob,sys,subprocess
-from SNIPRAPIFILES.download_with_session_ID import *
+from download_with_session_ID import *
 print(" I AM READY TO DOWNLOAD SCANS")
