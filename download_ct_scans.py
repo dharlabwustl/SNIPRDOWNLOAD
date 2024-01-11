72209e288c3507b8943084ec4bcb26789723fbb1
dharlabwustl <atulkumar@wustl.edu>
2024-01-12 00:12:57 +0800

    Commit 1

--- download_ct_scans.py
+++ download_ct_scans.py
@@ -7,4 +7,8 @@ XNAT_PASS=sys.argv[3]
 XNAT_HOST=sys.argv[4]
 ## download meta-data for a session:
 get_metadata_session(SESSION_ID,outputfile=os.path.join('/workinginput',str(SESSION_ID)+".csv"))
+## download xml for this session:
+args = arguments()
+args.stuff=['download_an_xmlfile_with_URIString',SESSION_ID, os.path.join('/workinginput',str(SESSION_ID)+".xml") ,'/workinginput']
+download_an_xmlfile_with_URIString(args) #outputfiles_present=$(python3 /software/download_with_session_ID.py "${download_an_xmlfile_with_URIString_arguments[@]}")
 
