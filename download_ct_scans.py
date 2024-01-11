bf10b877c9a5e1e93e3c99ae4c6fbdc58c18e623
dharlabwustl <atulkumar@wustl.edu>
2024-01-12 00:02:01 +0800

    Commit 1

--- download_ct_scans.py
+++ download_ct_scans.py
@@ -1,5 +1,10 @@
 import os,glob,sys,subprocess
 from download_with_session_ID import *
 print(" I AM READY TO DOWNLOAD SCANS")
+SESSION_ID=sys.argv[1]
+XNAT_USER=sys.argv[2]
+XNAT_PASS=sys.argv[3]
+XNAT_HOST=sys.argv[4]
 ## download meta-data for a session:
+get_metadata_session(SESSION_ID,outputfile=str(SESSION_ID)+".csv")
 
