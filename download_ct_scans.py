ef0040902622dd79ed5607f176330093957c66a2
dharlabwustl <atulkumar@wustl.edu>
2024-01-11 23:58:48 +0800

    Commit 1

--- download_ct_scans.py
+++ download_ct_scans.py
@@ -1,3 +1,5 @@
 import os,glob,sys,subprocess
 from download_with_session_ID import *
 print(" I AM READY TO DOWNLOAD SCANS")
+## download meta-data for a session:
+
--- script_to_call_main_program.sh
+++ script_to_call_main_program.sh
@@ -9,5 +9,6 @@ echo ${TYPE_OF_PROGRAM}::TYPE_OF_PROGRAM
 
 if [[ ${TYPE_OF_PROGRAM} == "DOWNLOAD_SCANS" ]]; then
   SESSION_ID=${1}
+  PROJECT_ID=${1}
   python3 /software/download_ct_scans.py   ${SESSION_ID} $XNAT_USER $XNAT_PASS $XNAT_HOST
 fi
